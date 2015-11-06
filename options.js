23436ba6edbeaf020864038a31d467062a7930db
Peter Gasper <petergasper@viralpoetry.org>
2015-11-06 10:14:59 +0100

    Chrome store pictures added, CSS style added to the option page

--- options.js
+++ options.js
@@ -15,23 +15,23 @@ function save_options() {
         // Notify that we saved.
         if (chrome.extension.lastError) {
             console.log('An error occurred: ' + chrome.extension.lastError.message);
-        } else {
-            show_result('Data has been successfully saved.');
         }
+        show_result('Data has been successfully saved.');
     });
 }
 
 function clear_storage() {
     //window.localStorage.clear();
     chrome.storage.local.clear();
-    show_result('Data has been cleared.');
+    //show_result('Data has been cleared.');
     // clear form
     document.getElementById("frm1").elements.item(0).value = '';
+    show_result('Seed has been removed.');
 }
 
 chrome.storage.local.get('passwd_salt', function(result) {
     if (!result.passwd_salt) {
-        // generate random salt....
+        // generate random salt and save it to the store
         document.getElementById("frm1").elements.item(0).value = transform_to_pass(CryptoJS.lib.WordArray.random(16));
         save_options();
     } else {
@@ -41,6 +41,3 @@ chrome.storage.local.get('passwd_salt', function(result) {
 
 document.getElementById('save').addEventListener('click', save_options);
 document.getElementById('clear').addEventListener('click', clear_storage);
-show_result('Please be patient, key derivation may take several seconds.');
-
-
